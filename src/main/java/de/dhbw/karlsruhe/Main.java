01704a593a05ce15afd43d6a37c3084f5bb59003
Paul Bader <paul@pbdr.de>
2025-03-26 18:39:34 +0100

    inital commit

---
+++ src/main/java/de/dhbw/karlsruhe/Main.java
@@ -0,0 +1,11 @@
+package de.dhbw.karlsruhe;
+
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+
+import java.security.Security;
+
+public class Main {
+    public static void main(String[] args) {
+        Security.addProvider(new BouncyCastleProvider());
+    }
+}
